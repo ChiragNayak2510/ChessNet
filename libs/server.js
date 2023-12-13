62db6a595d4845717caecd1a8367f6ca12ba1d1c
ChiragNayak2510 <chiragnayak251002@gmail.com>
2023-12-14 01:21:17 +0530

    second

--- libs/server.js
+++ libs/server.js
@@ -1,25 +1,28 @@
-
 const express = require('express');
 const app = express();
 const http = require('http');
 const server = http.createServer(app);
 const { Server } = require('socket.io');
 const PORT = 8080;
+const cors = require('cors');
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-const io = new Server({
-  cors: {
-    origin: ['https://localhost:3000'],
-  },
-});
+const io = new Server(server,{
+  cors : {
+    origin : '*',
+    methods : ["GET","POST"]
+  }
+}
+)
 
 io.on('connection', (socket) => {
   console.log(`User connected: ${socket.id}`);
 });
 
+
 server.listen(PORT, () => {
-  console.log(`Server listening on *${PORT}`);
+  console.log(`Server listening on *:${PORT}`);
 });
